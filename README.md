453eb29c1e557a71ed959ee0acf6d3594226ac88
rudes <jacobriddle94@gmail.com>
2019-01-27 23:16:45 -0500

    add registry settings and fix run_once

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # Clipable
 
+## Installation
+
+Edit run_once.bat.CHANGEME and then rename and execute
+
 ## Testing
 
 ```Batch
 cargo build
 dev_install.bat
 ```
-
-## Notes
-
-- need to store streamable credentials somewhere, probably registry
-- could also be service params
-- when a new .mp4 that's < 1gb hits the folder, upload to streamable
-- save streamable URL in a txt file with the .mp4 file name
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ mod clip_service {
 	use std::time::Duration;
 	use std::fs::OpenOptions;
 	use std::io::Write;
+	use winreg::RegKey;
+	use winreg::enums::HKEY_LOCAL_MACHINE;
 	use reqwest::{multipart, Client};
 	use serde::Deserialize;
 	use notify::{Watcher, RecursiveMode, watcher};
@@ -39,7 +41,7 @@ mod clip_service {
 	define_windows_service!(ffi_service_main, clip_srv);
 
 	pub fn clip_srv(_arguments: Vec<OsString>) {
-		if let Err(_e) = run_service() {
+		if let Err(_e) = run_service () {
 			// log error
 		}
 	}
@@ -47,7 +49,8 @@ mod clip_service {
 	pub fn run_service() -> Result<()> {
 		let (shut_tx, shut_rx) = mpsc::channel();
 		let (watcher_tx, watcher_rx) = mpsc::channel();
-		let folder = ;
+		let key = RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey("SOFTWARE\\Clipable").unwrap();
+		let folder: String = key.get_value("clipableFolder").unwrap();
 
 		let event_handler = move |control_event| -> ServiceControlHandlerResult {
 			match control_event {
@@ -64,7 +67,7 @@ mod clip_service {
 
 		let status_handle = service_control_handler::register(SERVICE_NAME, event_handler)?;
 		let mut watcher = watcher(watcher_tx, Duration::from_secs(2)).unwrap();
-		watcher.watch(folder, RecursiveMode::Recursive).unwrap();
+		watcher.watch(folder, RecursiveMode::NonRecursive).unwrap();
 
 		status_handle.set_service_status(ServiceStatus {
 			service_type: SERVICE_TYPE,
@@ -107,12 +110,13 @@ mod clip_service {
 		if path.is_none() {
 			return;
 		}
-		if path.unwrap().extension().unwrap() != ".mp4" {
+		if path.unwrap().extension().unwrap() != "mp4" {
 			return;
 		}
-		let username = ;
-		let password = ;
-		let folder = ;
+		let key = RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey("SOFTWARE\\Clipable").unwrap();
+		let username: String = key.get_value("clipableUsername").unwrap();
+		let password: String = key.get_value("clipablePassword").unwrap();
+		let folder: String = key.get_value("clipableFolder").unwrap();
 		let client = Client::new();
 		let filename = path.unwrap().file_name().unwrap().to_str().unwrap();
 		let full_filename = path.unwrap().to_str().unwrap();
@@ -122,7 +126,7 @@ mod clip_service {
 							 .multipart(form).send().unwrap();
 		let resJson: streamRes = response.json().unwrap();
 		let mut file = OpenOptions::new().append(true)
-										 .open(format!("{}{}", folder, "\\clipable.txt"))
+										 .open(format!("{}\\{}", folder, "clipable.txt"))
 										 .expect("Can't open file");
 		writeln!(file, "{} : https://streamable.com/{}", filename, resJson.shortcode).expect("Unable to write data");
 	}
