62411b27a3e53e6030e96490dd076712bcd6f630
rudes <jacobriddle94@gmail.com>
2021-07-04 20:51:15 -0400

    Error handling and MSI Installer
    using wix to create MSI Installer
    using winlog to log events to Application/clipable event viewer logs
    MSI installer has gui for recording streamable details

--- dev_install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-sc stop Clipable
-sc delete Clipable
-sc create Clipable start=auto binPath= %cd%"\target\release\clipable.exe"
-sc description Clipable "Uploads clips from Documents/Clipable to Streamable.com"
-sc start Clipable
-PAUSE
